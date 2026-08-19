25b276f605731c91556909c54e63df7d112066a3
Noah Drisner <driz@Noahs-MacBook-Pro.local>
2026-08-19 00:39:53 -0600

    Fixed DnD Calculator

--- DnDCalc.py
+++ DnDCalc.py
@@ -5,18 +5,18 @@ Hp = st.text_input("How many Hitpoints does the character have?")
 # Caculating Damage based on the following:
 Type = st.text_input("Resistance, Vulnerability, Immunity, or Straight? ")
 if Type == "Straight":
-    D = st.text_input("How much Damage?")
-    TD = (D)
+    D = st.number_input("How much Damage?")
+    TD =(D) 
 if Type == "Resistance":
     import math
     # D = Damage
-    D = st.text_input("How much Damage?")
+    D = st.number_input("How much Damage?")
     #TD = Total Damage
-    TD = math.floor((D/2))
+    TD = math.floor((D)/2)
 elif Type == "Vulnerability":
     import math
     #D = Damage
-    D = st.text_input("How much Damage?")
+    D = st.number_input("How much Damage?")
     #TD = Total Damage
     TD = math.floor((D)*2)
 elif Type == "Immunity":
